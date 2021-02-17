108555b115a720296929d7980e108d718131e531
meliodas1107 <57810820+meliodas1107@users.noreply.github.com>
2021-02-17 21:13:17 +0530

    Update debloat_script_personal.sh
    added to remove and disable some apps

--- Setup Scripts/debloat_script_personal.sh
+++ Setup Scripts/debloat_script_personal.sh
@@ -7,3 +7,7 @@ adb shell pm uninstall -k --user 0 com.heytap.market
 adb shell cmd package install-existing --user 0 com.coloros.gamespace
 adb shell pm uninstall -k --user 0 com.google.android.apps.youtube.music
 adb shell pm uninstall -k --user 0 com.coloros.phonemanager
+ adb shell pm disable-user --user 0  com.heytap.market
+ adb shell pm disable-user --user 0 com.heytap.themestore
+ adb shell pm uninstall --user 0 com.heytap.usercenter 
+ #adb shell pm uninstall --user 0 com.heytap.habit.analysis
